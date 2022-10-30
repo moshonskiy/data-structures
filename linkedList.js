66378a62085ab613ffd010733d23727a83fce4c1
Kirill <furiousbenjamin@gmail.com>
2022-10-30 22:53:50 +0300

    append, toArray methods implemented

--- linkedList.js
+++ linkedList.js
@@ -3,4 +3,31 @@ class LinkedList {
         this.head = null;
         this.tail = null;
     }
+
+    append(value) {
+        const newNode = { value, next: null };
+
+        if (this.tail) {
+            this.tail.next = newNode;
+        }
+
+        this.tail = newNode;
+
+        if (!this.head) {
+            this.head = newNode;
+        }
+    }
+
+    toArray() {
+        const arrayList = [];
+
+        let pointer = this.head;
+        while(pointer) {
+            arrayList.push(pointer);
+
+            pointer = pointer.next;
+        }
+
+        return arrayList;
+    }
 }
