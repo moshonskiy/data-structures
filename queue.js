6d69b70c3512cab39191acc68f0fcab4fd7aacfb
Kirill Moshonskiy <furiousbenjamin@gmail.com>
2022-11-08 22:58:53 +0300

    queue time complexity

--- queue.js
+++ queue.js
@@ -1,3 +1,12 @@
+//                                      time complexity
+//                              stack                               array
+
+// element access               O(1) limited to 1 element           O(1)
+// insertion at the end         O(n) with data loss                 O(1)
+// insertion at the beginning   O(1)                                O(n)
+// insertion in the middle      O(n) with data loss                 O(n)
+// search time                  O(n) with data loss                 O(n)
+
 class Queue {
     constructor() {
         this.items = [];
