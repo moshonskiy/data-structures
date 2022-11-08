4749476fa4b50e50030f2f69b22f6baa680ac3e5
Kirill Moshonskiy <furiousbenjamin@gmail.com>
2022-11-08 22:41:34 +0300

    stack time complexity

--- stack.js
+++ stack.js
@@ -1,3 +1,14 @@
+// array simplified, restricted options of interacting with it (only push and pop)
+
+//                                      time complexity
+//                              stack                  array
+
+// element access               O(1)                      O(1)
+// insertion at the end         O(1)                      O(1)
+// insertion at the beginning   O(n) with data loss       O(n)
+// insertion in the middle      O(n) with data loss       O(n)
+// search time                  O(n) with data loss       O(n)
+
 class Stack {
     constructor() {
         this.items = [];
