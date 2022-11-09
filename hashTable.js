0119f5b33060a0ba926cdeb5455a2adfc1204f17
Kirill Moshonskiy <furiousbenjamin@gmail.com>
2022-11-09 23:14:49 +0300

    add hashTable

---
+++ hashTable.js
@@ -0,0 +1,26 @@
+class HashTable {
+    constructor() {
+        this.size = 1000;
+        this.buckets = Array(1000).fill(null);
+    }
+
+    hash(key) {
+        let hash = 0;
+
+        for (const char of key) {
+            hash += char.charCodeAt(0);
+        }
+
+        return hash % this.size;
+    }
+
+    set(key, value) {
+        const keyHash = this.hash(key);
+        this.buckets[keyHash] = value;
+    }
+
+    get(key) {
+        const keyHash = this.hash(key);
+        return this.buckets[keyHash];
+    }
+}
